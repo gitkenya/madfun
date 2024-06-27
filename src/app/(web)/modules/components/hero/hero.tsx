9d29900731ec303436341e72a215c9a4d5bd85f2
Lexacle Technologies Ltd <lexacletechnologies@gmail.com>
2024-06-27 16:20:55 +0300

    Updated Hero Responsiveness

--- src/app/(web)/modules/components/hero/hero.tsx
+++ src/app/(web)/modules/components/hero/hero.tsx
@@ -5,22 +5,22 @@ export default function Hero() {
       <div className="mx-auto max-w-[90rem] px-2 md:px-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
           <div className="grid">
-            <div className="rounded-xl bg-pink-100 flex flex-row min-h-[18rem]">
-              <div className="w-full h-full sm:w-1/2 p-4">
-                Content Placeholder
-              </div>
+            <div className="rounded-xl bg-pink-100 flex flex-col sm:flex-row-reverse min-h-[18rem]">
               <div className="w-full h-full sm:w-1/2 relative">
                 <Image
-                  className="object-cover rounded-e-xl"
+                  className="object-cover rounded-t-xl sm:rounded-e-xl sm:rounded-t-none"
                   src="/assets/img/hero/travel.jpg"
                   fill
                   alt="Madfun"
                 />
               </div>
+              <div className="w-full h-full sm:w-1/2 p-4">
+                Content Placeholder
+              </div>
             </div>
           </div>
           <div className="grid">
-            <div className="rounded-xl flex flex-row min-h-[18rem] gap-6">
+            <div className="rounded-xl flex flex-col sm:flex-row min-h-[18rem] gap-6">
               <div className="w-full h-full sm:w-1/2 bg-slate-100 rounded-xl p-4">
                 Content Placeholder
               </div>
