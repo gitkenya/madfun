f4af9d105504a6a2aeb3385cb5f5f94b5b1259de
Lexacle Technologies Ltd <lexacletechnologies@gmail.com>
2024-06-27 16:12:09 +0300

    Added Packages Section

---
+++ src/app/(web)/modules/components/packages/packages.tsx
@@ -0,0 +1,68 @@
+import Link from "next/link";
+import Image from "next/image";
+import {
+  IoChevronForwardOutline,
+  IoEye,
+  IoEyeOutline,
+  IoShare,
+} from "react-icons/io5";
+import { IoMdShare } from "react-icons/io";
+
+export default function Packages() {
+  const events: any[] = [1, 2, 3];
+  return (
+    <section className="w-full py-8 bg-slate-100 mt-4">
+      <div className="mx-auto max-w-[90rem] px-2 md:px-4 space-y-16">
+        <div className="w-full flex flex-row items-center justify-between">
+          <h1 className="text-3xl font-bold">Holiday Packages</h1>
+          <Link
+            href="/"
+            className="text-sm font-medium underline flex flex-row items-center gap-2"
+          >
+            See All <IoChevronForwardOutline size={20} />
+          </Link>
+        </div>
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {events?.map((key) => (
+            <div key={key} className="grid">
+              <div className="bg-slate-50 p-4 rounded-xl flex flex-row justify-between gap-4">
+                <div className="relative w-full sm:w-1/2 rounded-xl">
+                  <Image
+                    src="/assets/img/packages/package1.svg"
+                    width={600}
+                    height={600}
+                    alt="Events in Kenya Cinema Nairobi"
+                  />
+                </div>
+                <div className="w-full sm:w-1/2 flex flex-col justify-between">
+                  <div>
+                    <h3 className="font-bold text-lg line-clamp-1">
+                      Burna Boy Live in Jo South Africa
+                    </h3>
+                    <p className="text-slate-500 text-sm">5 Nights</p>
+                  </div>
+                  <div className="flex flex-row justify-between text-slate-500">
+                    <span className="text-sm flex flex-row items-center gap-2">
+                      <IoEyeOutline size={20} /> 20
+                    </span>
+                    <IoMdShare size={20} />
+                  </div>
+                  <div className="flex flex-col gap-1">
+                    <h4 className="font-bold text-xl">Ksh 50,000</h4>
+                    <p className="text-sm text-red-600">Valid until 30 Sep</p>
+                    <Link
+                      href="/"
+                      className="px-5 py-2 block text-center bg-slate-800 text-slate-50 rounded"
+                    >
+                      Read more
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "@/app/(web)/modules/components/hero/hero";
 import Events from "@/app/(web)/modules/components/events/events";
+import Packages from "@/app/(web)/modules/components/packages/packages";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Events />
+      <Packages />
       Home
     </>
   );
