7f3808ff97454d5d3f48dcc7f3595335a85032e4
gitkenya <git.lexacle@gmail.com>
2024-10-14 21:58:21 +0300

    Updated Flight Search Module

--- src/app/(web)/flights/modules/flights.tsx
+++ src/app/(web)/flights/modules/flights.tsx
@@ -1,3 +1,13 @@
+import { IoEllipse } from "react-icons/io5";
+
 export default function Flights() {
-  return <>Flights Form</>;
+  return (
+    <div className="w-full max-w-[56rem] flex flex-col gap-4">
+      <h1 className="flex flex-row items-end gap-3 font-poppins text-lg sm:text-4xl uppercase font-bold text-white tracking-wider">
+        Flights
+        <IoEllipse size={14} className="text-yellow-400 mb-[0.4rem]" />
+      </h1>
+      <div className="w-full p-4 rounded-lg bg-white"></div>
+    </div>
+  );
 }
--- src/app/(web)/flights/page.tsx
+++ src/app/(web)/flights/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import Flights from "./modules/flights";
 export default function Page() {
   return (
     <section className="w-full py-8">
       <div className="mx-auto max-w-[90rem] px-2 md:px-4 sm:pt-12">
         <div className="w-full min-h-[calc(100vh-100px)] bg-slate-800 rounded-xl relative">
-          <div className="absolute top-0 left-0 w-full h-full rounded-xl bg-gradient-to-b from-slate-900/50 to-slate-900/20 z-20"></div>
+          <div className="absolute flex flex-col items-center justify-center top-0 left-0 w-full h-full rounded-xl bg-gradient-to-b from-slate-900/60 via-slate-900/50 to-slate-900/20 z-20">
+            <Flights />
+          </div>
           <Image
             className="object-cover rounded-xl"
             src="/assets/img/flights/flights-banner.jpg"
