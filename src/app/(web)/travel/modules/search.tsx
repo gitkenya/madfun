069e76afd697dc9b60887eaf11283e9ea1630a4e
gitkenya <git.lexacle@gmail.com>
2024-10-15 20:42:14 +0300

    Updated Travel Search UI

--- src/app/(web)/travel/modules/search.tsx
+++ src/app/(web)/travel/modules/search.tsx
@@ -28,7 +28,7 @@ export default function SearchForm() {
         Travel Experiences
         <IoEllipse size={14} className="text-yellow-400 mb-[0.4rem]" />
       </h1>
-      <div className="grid grid-cols-3 p-4 rounded-lg bg-white">
+      <div className="grid grid-cols-3 rounded-lg bg-white">
         <div className="flex flex-row items-center border-r border-slate-200">
           <Select
             defaultValue={selectedLocation}
@@ -38,12 +38,12 @@ export default function SearchForm() {
             unstyled
             instanceId={useId()}
             components={{ DropdownIndicator: CustomDropdownIndicator }}
-            className="w-full"
+            className="w-full m-4"
             classNames={{
               control: () =>
                 "appearance-none flex items-center w-full h-full outline-none text-base text-slate-800 bg-white rounded py-2 px-4 focus:outline-none",
               menu: () =>
-                "p-1 -ms-4 mt-2 border w-full min-w-[200px] left-0  border-slate-200 text-slate-800 bg-white rounded-lg py-3 px-4 focus:outline-none",
+                "p-1 mt-2 border w-full left-0 border-slate-200 text-slate-800 bg-white rounded-lg py-3 px-4 focus:outline-none",
               option: () => "cursor-pointer p-2 hover:bg-slate-100 rounded-sm",
               menuList: () => "cursor-pointer text-base",
               placeholder: () => "text-base text-slate-600",
@@ -60,10 +60,10 @@ export default function SearchForm() {
             unstyled
             instanceId={useId()}
             components={{ DropdownIndicator: CustomDropdownIndicator }}
-            className="w-full"
+            className="w-full m-4"
             classNames={{
               control: () =>
-                "appearance-none flex items-center w-full h-full outline-none text-base text-slate-800 bg-white rounded py-2 px-4 focus:outline-none",
+                "appearance-none items-center w-full h-full outline-none text-base text-slate-800 bg-white rounded py-2 px-4 focus:outline-none",
               menu: () =>
                 "p-1 mt-2 border w-full min-w-[200px] left-0  border-slate-200 text-slate-800 bg-white rounded-lg py-3 px-4 focus:outline-none",
               option: () => "cursor-pointer p-2 hover:bg-slate-100 rounded-sm",
@@ -74,7 +74,7 @@ export default function SearchForm() {
           />
         </div>
         <div className="flex flex-row items-center justify-end">
-          <button className="transition-all duration-300 sm:min-w-[120px] bg-slate-800 hover:bg-slate-950 text-white rounded-lg px-4 py-3 text-center">
+          <button className="transition-all duration-300 m-4 sm:min-w-[120px] bg-slate-800 hover:bg-slate-950 text-white rounded-lg px-4 py-3 text-center">
             Search
           </button>
         </div>
