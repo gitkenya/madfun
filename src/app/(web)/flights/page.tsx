752ba4cd12617923229d51a252b67d716387f414
gitkenya <git.lexacle@gmail.com>
2024-10-17 16:37:22 +0300

    Updated Main Flight Search UI

--- src/app/(web)/flights/page.tsx
+++ src/app/(web)/flights/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
-import Flights from "./modules/flights";
+import FlightSearch from "@/app/(web)/modules/components/search/flights";
 import { createClient } from "@/utils/supabase/server";
+import { IoEllipse } from "react-icons/io5";
 export const revalidate = 604800;
 export default async function Page() {
   const supabase = await createClient();
@@ -25,12 +26,18 @@ export default async function Page() {
       <div className="mx-auto max-w-[85rem] px-2 sm:px-0 pt-10 sm:pt-12">
         <div className="w-full h-60 sm:min-h-[calc(100vh-100px)] bg-slate-800 rounded-xl relative">
           <div className="absolute flex flex-col items-center justify-center top-0 left-0 w-full h-full rounded-xl bg-gradient-to-b from-slate-900/60 via-slate-900/50 to-slate-900/20 z-20">
-            <Flights
-              flight_types={flight_types}
-              location_types={locations}
-              flight_stops={flight_stops}
-              flight_classes={flight_classes}
-            />
+            <div className="w-full max-w-[75rem] hidden sm:flex flex-col gap-4">
+              <h1 className="flex flex-row items-end gap-3 font-poppins text-lg sm:text-4xl uppercase font-bold text-white tracking-wider">
+                Flights
+                <IoEllipse size={14} className="text-yellow-400 mb-[0.4rem]" />
+              </h1>
+              <FlightSearch
+                flight_types={flight_types}
+                location_types={locations}
+                flight_stops={flight_stops}
+                flight_classes={flight_classes}
+              />
+            </div>
           </div>
           <Image
             className="object-cover rounded-xl"
