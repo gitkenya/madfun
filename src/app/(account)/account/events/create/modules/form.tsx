8a1edc7b47ec1e7e9b0013884c587c42dd54e06f
gitkenya <git.lexacle@gmail.com>
2024-10-21 14:56:33 +0300

    Added Date Picker

--- src/app/(account)/account/events/create/modules/form.tsx
+++ src/app/(account)/account/events/create/modules/form.tsx
@@ -9,11 +9,16 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useAccount } from "@/providers/account/account";
 import Mockup from "./mockup";
 import { IoCloseOutline } from "react-icons/io5";
+import Datepicker from "react-tailwindcss-datepicker";
 
 export default function CreateEventForm(props: any) {
   const [step, setStep] = useState(1);
   const { newEventData, setNewEventData, openEventDrawer, setOpenEventDrawer } =
     useAccount();
+  const [eventDate, setEventDate] = useState<any>({
+    startDate: null,
+    endDate: null,
+  });
   const handleNext = () => {
     setStep((prevStep) => prevStep + 1);
   };
@@ -60,7 +65,7 @@ export default function CreateEventForm(props: any) {
   };
 
   return (
-    <div className="w-full h-full flex flex-col sm:flex-row gap-4 sm:gap-6 overflow-x-hidden">
+    <div className="w-full h-[calc(100vh-64px)] flex flex-col sm:flex-row gap-4 sm:gap-6 overflow-x-hidden">
       <div className="p-4 w-full sm:w-3/5 min-h-[calc(100vh-64px)]">
         <div>
           {step === 1 && (
@@ -86,12 +91,12 @@ export default function CreateEventForm(props: any) {
           />
         </div>
       </div>
-      <div className="w-full sticky top-[64px] h-[calc(100vh-64px)] hidden sm:flex flex-col items-center justify-center sm:w-2/5 sm:border-l sm:border-slate-200 dark:border-slate-800">
+      <div className="w-full sticky top-0 h-[calc(100vh-64px)] hidden sm:flex flex-col items-center justify-center sm:w-2/5 sm:border-l sm:border-slate-200 dark:border-slate-800">
         <div className="w-full h-full relative sm:flex flex-col items-center justify-center">
           <AnimatePresence>
             {openEventDrawer && (
               <motion.div
-                className="absolute w-full h-full bg-slate-100 z-50"
+                className="absolute w-full h-full bg-slate-100 dark:bg-slate-800 z-50"
                 initial="hidden"
                 animate="visible"
                 exit="exit"
@@ -104,6 +109,41 @@ export default function CreateEventForm(props: any) {
                 >
                   <IoCloseOutline size={20} />
                 </button>
+                <div className="w-full h-full text-slate-600 dark:text-slate-300  pt-12">
+                  <div className="px-6">
+                    <h2 className="text-lg font-semibold text-slate-600 dark:text-slate-300 mb-5">
+                      Add event date & time
+                    </h2>
+                    <div className="flex flex-col gap-4">
+                      <div className="flex flex-col gap-1">
+                        <label
+                          htmlFor="event_date"
+                          className="uppercase text-sm text-slate-600 dark:text-slate-300"
+                        >
+                          Date
+                        </label>
+                        <Datepicker
+                          primaryColor={"red"}
+                          inputClassName={`appearance-none relative block w-full outline-none text-sm border border-slate-200 dark:border-slate-700 text-slate-700 dark:text-slate-300 bg-slate-50 dark:bg-slate-800 rounded-lg py-3 px-4 focus:outline-none`}
+                          toggleClassName="absolute bg-slate-200 dark:bg-slate-700 rounded-r-lg text-slate-600 dark:text-slate-300 right-0 top-0 h-full px-3 text-gray-400 focus:outline-none disabled:opacity-40 disabled:cursor-not-allowed"
+                          useRange={false}
+                          asSingle={true}
+                          value={eventDate}
+                          displayFormat="DD-MM-YYYY"
+                          onChange={(newDate) => setEventDate(newDate)}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="absolute bottom-0 w-full flex flex-row justify-between gap-6 p-4 px-6 bg-slate-50 dark:bg-slate-900">
+                    <button className="flex flex-row justify-center w-full bg-slate-50 dark:bg-transparent border border-slate-700 dark:border-slate-600 text-slate-800 dark:text-slate-400 px-4 py-2 rounded-lg">
+                      Clear
+                    </button>
+                    <button className="flex flex-row justify-center w-full bg-slate-800 dark:bg-slate-700 border border-slate-800 text-slate-50 dark:text-slate-300 px-4 py-2 rounded-lg">
+                      Save & Continue
+                    </button>
+                  </div>
+                </div>
               </motion.div>
             )}
           </AnimatePresence>
