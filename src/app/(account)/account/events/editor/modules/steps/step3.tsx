924395816d86868d26df4ec8536d2b8f15719ee9
gitkenya <git.lexacle@gmail.com>
2024-10-23 11:50:52 +0300

    Updated Create Event Step 3

--- src/app/(account)/account/events/editor/modules/steps/step3.tsx
+++ src/app/(account)/account/events/editor/modules/steps/step3.tsx
@@ -1,5 +1,7 @@
 // components/Step3.tsx
 import React from "react";
+import Image from "next/image";
+import { IoAddOutline } from "react-icons/io5";
 
 const Step3 = ({ formData, handleChange }: any) => {
   return (
@@ -17,6 +19,26 @@ const Step3 = ({ formData, handleChange }: any) => {
               Create and customize your event tickets by setting types, prices,
               quantities, etc
             </p>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-full  bg-slate-100 dark:bg-slate-700 rounded-lg flex flex-col items-center justify-center gap-5 py-6 sm:py-16">
+              <Image
+                src="/assets/img/icons/account/ticket3.png"
+                width={120}
+                height={120}
+                alt="Event Tickets"
+              />
+              <div className="flex flex-col items-center justify-center">
+                <h3 className="font-semibold text-slate-800 dark:text-slate-300 text-lg sm:text-xl">
+                  No Tickets Yet!
+                </h3>
+                <p className="">Create the tickets now</p>
+                <button className="transition-all duration-300 mt-4 flex flex-row items-center gap-2 px-6 py-2 rounded-lg border border-slate-700 text-slate-700 dark:border-slate-400 dark:text-slate-400 hover:bg-slate-800 hover:text-white">
+                  <IoAddOutline size={20} />
+                  Add Ticket
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
